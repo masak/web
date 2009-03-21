cfdbe944e17df405cc6b2868ba0dba93c9e68e55
Stephen Weeks <tene@allalone.org>
2009-03-21 00:23:36 -0600

    [Tags]
    Dynamically generate the list of tag subs.
    Rename from Web::Tags to Tags.

---
+++ lib/Tags.pm
@@ -0,0 +1,75 @@
+use v6;
+
+module Tags::EXPORT::DEFAULT { }
+
+module Tags {
+    our @frames;
+
+    # Hide it in a sub to work around a bug
+    sub _setup {
+        for <
+        h1 h2 h3 h4 h5 h6 p br hr ol ul li dl dt dd menu code var strong em tt
+        u i b blockquote pre img a address cite samp dfn html head base body
+        link nextid title meta kbd start_html end_html input select option
+        comment charset escapehtml div table caption th td tr tr sup sub
+        strike applet param nobr embed basefont style span layer ilayer font
+        frameset frame script small big area map abbr acronym bdo col colgroup
+        del fieldset iframe ins label legend noframes noscript object optgroup
+        q thead tbody tfoot blink fontsize center textfield textarea filefield
+        password_field hidden checkbox checkbox_group submit reset defaults
+        radio_group popup_menu button autoescape scrolling_list image_button
+        start_form end_form startform endform start_multipart_form
+        end_multipart_form isindex tmpfilename uploadinfo url_encoded
+        multipart form canvas
+        > -> $tag {
+            ::Tags{$tag} = sub (&c) {
+                _tag($tag, &c);
+            }
+            ::Tags::EXPORT::DEFAULT{$tag} = ::Tags{$tag};
+        }
+    }
+    _setup();
+
+    sub show(&code) is export(:DEFAULT) {
+        new_buffer_frame();
+        &code();
+        return end_buffer_frame();
+    }
+
+    sub _tag(Str $tag, &code) {
+        my $buf = "\n" ~ '  ' x (@frames.elems() - 1) ~ "<$tag>";
+        new_buffer_frame();
+        my $ret = &code();
+        my $frame = end_buffer_frame();
+        if $frame.chars() > 0 {
+            $buf ~= $frame;
+        }
+        else {
+            $buf ~= $ret;
+        }
+        $buf ~= "\n" ~ '  ' x (@frames.elems() - 1) ~ "</$tag>";
+        outs($buf);
+        return '';
+    }
+
+    sub outs($text) is export(:DEFAULT) {
+        @frames[0] ~= ($text);
+    }
+
+    sub new_buffer_frame {
+        @frames.unshift('');
+    }
+
+    sub end_buffer_frame {
+        @frames.shift();
+    }
+
+    class Web::Tags::Buffer {
+        has $.data is rw;
+        method append($text) {
+            $.data ~= $text;
+        }
+    }
+
+}
+
