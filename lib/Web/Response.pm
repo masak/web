5afd756849a6b2896e14c289ac64d0d272e647cd
Carl Masak <cmasak@gmail.com>
2009-04-09 16:51:55 +0200

    [Web::Response] ported some from Rack

--- lib/Web/Response.pm
+++ lib/Web/Response.pm
@@ -1,2 +1,19 @@
 class Web::Response {
+    has $.length is rw;
+    has %!header;
+    has @!body;
+    has &!writer = { @!body.push($^x) };
+
+    # Append to body and update Content-Length.
+    #
+    # NOTE: Do not mix #write and direct #body access!
+    #
+    # RAKUDO: '$str as Str'
+    method write(Str $str) {
+        $!length += $str.chars;
+        &!writer($str);
+
+        %!header<Content-Length> = ~$!length;
+        return $str;
+    }
 }
