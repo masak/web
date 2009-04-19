d10c4507a83178bd525e1c81a410a51fe74e8c3b
Ilya Belikin <ihrd@lenovo.csmile.ru>
2009-04-19 23:45:10 +1100

    Routes: add some draft .dispatch($request) method, addd %param to .dispatch and .apply merge it with %!args when call code

--- lib/Routes.pm
+++ lib/Routes.pm
@@ -13,6 +13,7 @@ multi method add (@pattern, Code $code) {
     @!routes.push: Routes::Route.new( pattern => @pattern, code => $code, | %_);
 }
 
+# draft
 method connect (@pattern, *%_ is rw) {
     %_<controller> //= 'Root';
     %_<action> //= 'index';
@@ -20,11 +21,15 @@ method connect (@pattern, *%_ is rw) {
     @!routes.push: Routes::Route.new( pattern => @pattern, | %_ );
 }
 
-method dispatch (@chunks) {
+# I think it should work as I mean without this one
+multi method dispatch (@chunks) { self.dispatch(@chunks, Hash.new) }
+
+multi method dispatch (@chunks, %param) {
+#multi method dispatch (@chunks, %param?) {
     my @matched =  @!routes.grep: { .match(@chunks) };    
     
     if @matched {
-        my $result = @matched[*-1].apply;
+        my $result = @matched[*-1].apply(%param);
         .clear for @!routes; 
         return $result;
     }
@@ -36,4 +41,18 @@ method dispatch (@chunks) {
     }
 }
 
+# draft
+multi method dispatch ($request) {
+    my %param;
+    %param<request> = $request;
+    %param<post>    = $request.POST;
+    %param<get>     = $request.GET;
+
+    # Use param as method first because of HTML4 do not support PUT and DELETE 
+    %param<method> = %param<post><request_method> || $request.request_method;
+
+    # Do not find this .path-chunks in rack request object, 
+    # but I hope we will add something like this with chunks from URI.pm
+    self.dispatch($request.path-chunks, %param);
+}
 # vim:ft=perl6
