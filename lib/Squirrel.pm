5e7f9698f04eb30745b73a3efc4f0332b748fdb4
Carl Masak <cmasak@gmail.com>
2009-09-09 22:34:08 +0200

    hooked up Squirrel to SQLite3
    First test now passes.

--- lib/Squirrel.pm
+++ lib/Squirrel.pm
@@ -1,12 +1,38 @@
+use SQLite3;
+
 class Squirrel::Dataset {
     method insert(*@values) {
     }
 }
 
 class Squirrel::Database {
-    has $.file;
+    has $!file;
+    has $!dbh;
+
+    submethod BUILD(:$file!) {
+        $!file = $file; # RAKUDO: This shouldn't be needed
+    }
+
+    method open() {
+        $!dbh = sqlite_open($!file);
+    }
+
+    method close() {
+        $!dbh.close();
+    }
+
+    method exec($statement) {
+        my $sth = $!dbh.prepare($statement);
+        $sth.step();
+        $sth.finalize();
+    }
 
     method create_table(*@args) {
+        given self {
+            .open;
+            .exec('CREATE TABLE foo (item,count)');
+            .close;
+        }
     }
 
     method from($table) {
