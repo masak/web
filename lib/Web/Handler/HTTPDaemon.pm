e24dabb7e5af79e446e4dc4107eb4a389313925f
Carl Masak <cmasak@gmail.com>
2009-10-24 09:58:24 +0200

    [Web::Handler::HTTPDaemon] generalized the call
    Now not only the port, but also the host name is a parameter.

--- lib/Web/Handler/HTTPDaemon.pm
+++ lib/Web/Handler/HTTPDaemon.pm
@@ -3,8 +3,8 @@ use v6;
 use HTTP::Daemon;
 
 class Web::Handler::HTTPDaemon {
-    method run(&app, :$port = 8888) {
-        my HTTP::Daemon $d .= new(LocalPort => $port);
+    method run(&app, :$host = 'localhost', :$port = 8888) {
+        my HTTP::Daemon $d .= new(LocalAddr => $host, LocalPort => $port);
         while my $c = $d.accept and my HTTP::Request $r = $c.get_request {
             my $qs = $r.url.path ~~ / '?' (.*) $/ ?? $0 !! '';
             my %env = { "QUERY_STRING" => $qs };
